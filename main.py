151c8615081a03c2134d1df85fa89e91721a78b4
dmitry <odnolko.dk@phystech.edu>
2021-03-20 20:19:50 +0300

    bug produced

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from flask import Flask, url_for, render_template
-import sources
+from flask import Flask, render_template
 app = Flask('parking-recognition', template_folder="jinja_templates")
 
 
 @app.route('/')
 def start():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
