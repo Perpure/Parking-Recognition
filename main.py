527cca024f3d2e9eae3ddffb83772527bde2c03c
Paul Dyachek <dyachekp@gmail.com>
2021-03-21 12:18:53 +0300

    getting proper cars on praking

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
 from imageai.Detection import ObjectDetection
 import numpy as np
 import cv2
-<<<<<<< HEAD
-import mrcnn.config
-import mrcnn.utils
-from mrcnn.model import MaskRCNN
-from pathlib import Path
-from park_calc import cut_parking, PARKS_POLYGONS
-
-class MaskRCNNConfig(mrcnn.config.Config):
-    NAME = "coco_pretrained_model_config"
-    IMAGES_PER_GPU = 1
-    GPU_COUNT = 1
-    NUM_CLASSES = 1 + 80
-    DETECTION_MIN_CONFIDENCE = 0.25
-
-def get_car_boxes(boxes, class_ids):
-=======
 import os
 import time
+from park_calc import PARKS_POLYGONS, cut_parking
 
 execution_path = os.getcwd()
 
 def get_car_boxes(frame):
->>>>>>> car-recognition
     car_boxes = []
     detections = (detector.detectCustomObjectsFromImage(custom_objects=custom, input_type="array", input_image=np.array(frame), output_type="array", minimum_percentage_probability=20))[1]
 
@@ -33,19 +17,7 @@ def get_car_boxes(frame):
     return np.array(car_boxes)
 
 
-<<<<<<< HEAD
-ROOT_DIR = Path(".")
-MODEL_DIR = ROOT_DIR / "logs"
-COCO_MODEL_PATH = ROOT_DIR / "mask_rcnn_coco.h5"
-
-if not COCO_MODEL_PATH.exists():
-    mrcnn.utils.download_trained_weights(COCO_MODEL_PATH)
-
-IMAGE_DIR = ROOT_DIR / "images"
-VIDEO_SOURCE = "test_images/parking3.flv"
-=======
-VIDEO_SOURCE = "test_images/parking1.mp4"
->>>>>>> car-recognition
+VIDEO_SOURCE = "test_images/parking1.flv"
 
 detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
@@ -69,47 +41,25 @@ while video_capture.isOpened():
 
     t0 = time.time()
 
-<<<<<<< HEAD
-    r = results[0]
-
-    if parked_car_boxes is None:
-        parked_car_boxes = get_car_boxes(r['rois'], r['class_ids'])
-    else:
-        car_boxes = get_car_boxes(r['rois'], r['class_ids'])
-        parked_cars = cut_parking(car_boxes, PARKS_POLYGONS[0])
-        overlaps = mrcnn.utils.compute_overlaps(parked_car_boxes, car_boxes)
-        for parking_area, overlap_areas in zip(parked_car_boxes, overlaps):
-            max_IoU_overlap = np.max(overlap_areas)
-
-            y1, x1, y2, x2 = parking_area
-
-=======
     rgb_image = frame[:, :, ::-1]
 
-    car_boxes = get_car_boxes(rgb_image) #координаты коробок с машинами 
->>>>>>> car-recognition
+    car_boxes = get_car_boxes(rgb_image) #координаты коробок с машинами
+    car_boxes = cut_parking(car_boxes, PARKS_POLYGONS[0])
 
-    '''
     #если понадобится выводить на экран обработанное изображение и информацию про каждый объект в консоль, то можно вместо вызова get_car_boxes вставить этот код
-    
-    detections = detector.detectCustomObjectsFromImage(custom_objects=custom, input_type="array", input_image=np.array(rgb_image), output_type="array", minimum_percentage_probability=20)
-    cv2.imshow('Video', detections[0]) #перепутаны синий и красный каналы
 
-<<<<<<< HEAD
-        for car in parked_cars:
-            y1, x1, y2, x2 = parking_area
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
-        cv2.imshow('Video', frame)
-=======
-    for eachObject in detections[1]:
-        print(eachObject["name"] , " : " , eachObject["percentage_probability"], " : ", eachObject["box_points"])
->>>>>>> car-recognition
+    print(car_boxes)
+    for car in car_boxes:
+        x1, y1, x2, y2 = car
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
+
+    cv2.imshow('Video', frame) #перепутаны синий и красный каналы
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    '''
+
     spf = time.time() - t0
-    
+
 
 video_capture.release()
 cv2.destroyAllWindows()
--- park_calc.py
+++ park_calc.py
@@ -5,8 +5,16 @@ PARKS = [[(527, 569), (592, 515), (1412, 701), (1537, 815)]]
 
 PARKS_POLYGONS = [Polygon(x) for x in PARKS]
 
+def rect_area(arr):
+    x1, y1, x2, y2 = arr
+    return (x2 - x1) * (y2 - y1)
+
+def width(arr):
+    x1, y1, x2, y2 = arr
+    return x2 - x1
+
 def make_polygon(car):
-    y1, x1, y2, x2 = car
+    x1, y1, x2, y2 = car
     return Polygon([(x1, y1), (x1, y2), (x2, y2), (x2, y1)])
 
 def cut_parking(cars, park):
@@ -14,6 +22,25 @@ def cut_parking(cars, park):
     for car in cars:
         car_poly = make_polygon(car)
         intersec = car_poly.intersection(park).area
-        if intersec / car_poly.area > 0.6:
+        if intersec / car_poly.area > 0.4:
             parked_cars.append(car)
-    return np.array(parked_cars)
+    if parked_cars == []:
+        return np.array([])
+    parked_cars = np.array(parked_cars)
+    parked_cars = parked_cars[parked_cars[:, 0].argsort()]
+    med = np.median(np.apply_along_axis(rect_area, 1, parked_cars))
+    delta = np.median(np.apply_along_axis(width, 1, parked_cars)) * 0.3
+    answer = []
+    i = 0
+    while i < len(parked_cars) - 1:
+        x1, _, _, _ = parked_cars[i, :]
+        x2, _, _, _ = parked_cars[i + 1, :]
+        if (x2 - x1 < delta):
+            if (abs(rect_area(parked_cars[i]) - med) <
+                abs(rect_area(parked_cars[i + 1]) - med)):
+                parked_cars = np.delete(parked_cars, i + 1, 0)
+            else:
+                parked_cars = np.delete(parked_cars, i, 0)
+        else:
+            i += 1
+    return parked_cars
