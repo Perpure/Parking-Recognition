adc1fe0dee409e7a99bbe2bcb09ba18dd77e7f10
Paul Dyachek <dyachekp@gmail.com>
2021-03-21 16:13:31 +0300

    detecting parking on 1st cam

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import numpy as np
 import cv2
 import os
 import time
-from park_calc import PARKS_POLYGONS, cut_parking
+from park_calc import find_space, cut_parking
 
 execution_path = os.getcwd()
 
+
 def get_car_boxes(frame):
     car_boxes = []
-    detections = (detector.detectCustomObjectsFromImage(custom_objects=custom, input_type="array", input_image=np.array(frame), output_type="array", minimum_percentage_probability=20))[1]
+    detections = (detector.detectCustomObjectsFromImage(custom_objects=custom, input_type="array", input_image=np.array(frame), output_type="array", minimum_percentage_probability=18))[1]
 
     for eachObject in detections:
         car_boxes.append(eachObject["box_points"])
@@ -24,7 +25,7 @@ detector.setModelTypeAsRetinaNet()
 detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.1.0.h5"))
 detector.loadModel()
 
-custom = detector.CustomObjects(bicycle=True, car=True, motorcycle=True, bus=True, truck=True, boat=True, bear=True) #объекты, которые должна искать нейронка
+custom = detector.CustomObjects(bicycle=True, car=True, motorcycle=True, bus=True, truck=True, boat=True, bear=False) #объекты, которые должна искать нейронка
 
 video_capture = cv2.VideoCapture(VIDEO_SOURCE)
 
@@ -44,7 +45,17 @@ while video_capture.isOpened():
     rgb_image = frame[:, :, ::-1]
 
     car_boxes = get_car_boxes(rgb_image) #координаты коробок с машинами
-    car_boxes = cut_parking(car_boxes, PARKS_POLYGONS[0])
+
+    for car in car_boxes:
+        x1, y1, x2, y2 = car
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+
+    car_boxes = cut_parking(car_boxes, 0)
+    spaces = find_space(car_boxes, 0)
+    for space in spaces:
+
+        x, y = space
+        cv2.rectangle(frame, (x, y), (x, y), (0, 0, 255), 20)
 
     #если понадобится выводить на экран обработанное изображение и информацию про каждый объект в консоль, то можно вместо вызова get_car_boxes вставить этот код
 
--- park_calc.py
+++ park_calc.py
@@ -1,7 +1,10 @@
 import numpy as np
 from shapely.geometry import Polygon
 
-PARKS = [[(527, 569), (592, 515), (1412, 701), (1537, 815)]]
+PARKS = [[(527, 569), (592, 515), (1412, 701), (1537, 815)], #1
+         [(200, 389), (263, 367), (884, 530), (802, 596)]] #2 left
+
+PIX_PER_CAR = [75]
 
 PARKS_POLYGONS = [Polygon(x) for x in PARKS]
 
@@ -10,14 +13,28 @@ def rect_area(arr):
     return (x2 - x1) * (y2 - y1)
 
 def width(arr):
-    x1, y1, x2, y2 = arr
+    x1, _, x2, _ = arr
     return x2 - x1
+
+def height(arr):
+    _, y1, _, y2 = arr
+    return y2 - y1
+
+def center_rect(r):
+    x1, y1, x2, y2 = r
+    return x1 + (x2 - x1) / 2, y1 + (y2 - y1) / 2
+
+def dist_dot(d1, d2):
+    x1, y1 = d1
+    x2, y2 = d2
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
 def make_polygon(car):
     x1, y1, x2, y2 = car
     return Polygon([(x1, y1), (x1, y2), (x2, y2), (x2, y1)])
 
-def cut_parking(cars, park):
+def cut_parking(cars, id):
+    park = PARKS_POLYGONS[id]
     parked_cars = []
     for car in cars:
         car_poly = make_polygon(car)
@@ -29,13 +46,13 @@ def cut_parking(cars, park):
     parked_cars = np.array(parked_cars)
     parked_cars = parked_cars[parked_cars[:, 0].argsort()]
     med = np.median(np.apply_along_axis(rect_area, 1, parked_cars))
-    delta = np.median(np.apply_along_axis(width, 1, parked_cars)) * 0.3
-    answer = []
+    delta_w = np.median(np.apply_along_axis(width, 1, parked_cars)) * 0.3
+    delta_h = np.median(np.apply_along_axis(height, 1, parked_cars)) * 0.3
     i = 0
     while i < len(parked_cars) - 1:
-        x1, _, _, _ = parked_cars[i, :]
-        x2, _, _, _ = parked_cars[i + 1, :]
-        if (x2 - x1 < delta):
+        x1, y1, _, _ = parked_cars[i, :]
+        x2, y2, _, _ = parked_cars[i + 1, :]
+        if (x2 - x1 < delta_w) and (y2 - y1 < delta_h):
             if (abs(rect_area(parked_cars[i]) - med) <
                 abs(rect_area(parked_cars[i + 1]) - med)):
                 parked_cars = np.delete(parked_cars, i + 1, 0)
@@ -44,3 +61,53 @@ def cut_parking(cars, park):
         else:
             i += 1
     return parked_cars
+
+def find_space_between_cars(d1, d2, delta):
+
+    x, y = d1
+    xf, yf = d2
+    spaces = np.empty(shape=[0, 2], dtype=int)
+    dist = dist_dot((x, y), (xf, yf))
+    vec_x = (xf - x) / dist
+    vec_y = (yf - y) / dist
+    n = int(dist // delta)
+    print(d1, d2)
+    if n == 0:
+        return spaces
+    step = dist / n / 2
+    buf = np.empty(shape=[1, 2], dtype=int)
+    for _ in range(n):
+        x += vec_x * step
+        y += vec_y * step
+        buf[0] = int(x), int(y)
+        spaces = np.concatenate((spaces, buf))
+        x += vec_x * step
+        y += vec_y * step
+    print(spaces)
+    return spaces
+
+def find_space(cars, id):
+    park = PARKS[id]
+    delta = PIX_PER_CAR[id]
+    spaces = np.empty(shape=[0, 2], dtype=int)
+    x, y = park[0]
+    car = cars[0, :]
+    x1, y1, x2, y2 = car
+    xf, yf = x1, y1 + (y2 - y1) / 2
+    spaces = np.concatenate((spaces, find_space_between_cars((x, y), (xf, yf), delta)))
+    for i in range(len(cars) - 1):
+        car1 = cars[i, :]
+        car2 = cars[i + 1, :]
+        x1, y1, x2, y2 = car1
+        x3, y3, x4, y4 = car2
+        x, y = x2, y1 + (y2 - y1) / 2
+        xf, yf = x3, y3 + (y4 - y3) / 2
+        spaces = np.concatenate((spaces, find_space_between_cars((x, y), (xf, yf), delta)))
+
+
+    xf, yf = park[2]
+    car = cars[len(cars) - 1, :]
+    x1, y1, x2, y2 = car
+    x, y = x2, y1 + (y2 - y1) / 2
+    spaces = np.concatenate((spaces, find_space_between_cars((x, y), (xf, yf + 40), delta)))
+    return spaces
