2733e10bb838434834589c7e5d6fcf758a560ba3
Paul Dyachek <dyachekp@gmail.com>
2021-04-03 16:14:59 +0300

    tech pages

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import time
 from park_calc import find_space, cut_parking
 import tensorflow as tf
-from flask import Flask, render_template, Response, request, flash, redirect, url_for, send_from_directory
+from flask import Flask, render_template, Response, request, flash, redirect, url_for, send_from_directory, make_response
 from werkzeug.utils import secure_filename
 from markupsafe import escape
 from threading import Thread
@@ -36,6 +36,18 @@ def stream1():
     """Livestream of the 1st camera"""
     return render_template("stream1.html")
 
+@app.route('/get_spaces1')
+def get_spaces1():
+    response = make_response(str(free_parks[0]), 200)
+    response.mimetype = "text/plain"
+    return response
+
+
+@app.route('/get_frame1')
+def get_frame1():
+    response = make_response(read_frame(0), 200)
+    response.mimetype = "image/jpeg"
+    return response
 
 # @app.route('/stream2')
 # def stream2():
@@ -95,6 +107,7 @@ def gen(id):
 
     global frame_read_mode
     global frames
+    global free_parks
     while video.isOpened():
             success, frame = video.read()
             if not success:
@@ -109,7 +122,6 @@ def gen(id):
             elif video_spf > spf:
                 time.sleep(video_spf - spf)
 
-            print('processing')
             rgb_image = frame[:, :, ::-1]
 
             car_boxes = get_car_boxes(rgb_image)
@@ -139,14 +151,12 @@ def gen(id):
             frames[id] = img
             frame_read_mode[id] = True
             spf = time.time() - t0
-            print('processed')
 
 def read_frame(cam):
     while True:
         global frame_read_mode
         if (frame_read_mode[cam]) and (frames[cam] != None):
             frame_read_mode[cam] = False
-            print('readed')
             return frames[cam]
 
 def get_frame(cam):
